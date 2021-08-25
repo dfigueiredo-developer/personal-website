c0abb11e0907fdbed8cdca84b6cc4eb37e13265b
dfigueiredo-developer <dfigueiredo.developer@gmail.com>
2021-08-26 00:15:30 +0100

    Navbar impovements layout without responsiveness.

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,15 +1,27 @@
+import './Navbar.css';
 import items from './Items';
+
+window.onscroll = () => {
+     var navbar = document.getElementById('navbar-wrapper');
+     if(window.scrollY > 100)
+          navbar.classList.add('bg-white');
+     else
+          navbar.classList.remove('bg-white');
+}
 
 const Navbar = () => {
      return (
           
-          <div>
-               <span className = 'bold'>Diogo Figueiredo</span>
-               {
-                    items.map((item) => {
-                         return (<a key = {item.id} href = {item.anchor} className = 'bold text-decoration-none'>{item.label}</a>) 
-                    })
-               }
+          <div id = 'navbar-wrapper'>
+               <span className = 'navbar-logo'>Diogo Figueiredo</span>
+
+               <span className = 'navbar-items'>
+                    {
+                         items.map((item) => {
+                              return (<a key = {item.id} href = {item.anchor} className = 'bold text-decoration-none'>{item.label}</a>) 
+                         })
+                    }
+               </span>
           </div>
       );
 }
